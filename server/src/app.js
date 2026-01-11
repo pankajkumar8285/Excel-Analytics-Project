156d7fea9bda2a4f9aa8a38b9cb47e6256093ed2
Pankaj Kumar <168230102+pankajkumar8285@users.noreply.github.com>
2026-01-11 19:14:41 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from 'cookie-parser';
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cors({
   origin:"https://excel-analytics-project-1-79a0.onrender.com" ,
